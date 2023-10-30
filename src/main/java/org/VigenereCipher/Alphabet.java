f03b3b33d91208d4207f197200003d9dbe334af8
Aran <aransjannson@gmail.com>
2023-10-30 23:16:45 +0000

    Comments Added

--- src/main/java/org/VigenereCipher/Alphabet.java
+++ src/main/java/org/VigenereCipher/Alphabet.java
@@ -2,35 +2,70 @@ package org.VigenereCipher;
 
 import java.util.HashMap;
 
+
+/**
+ * Generate and return the standard and inverse Alphabet
+ *
+ * @author Aran Jannson
+ */
+
 public class Alphabet {
 
+    //HashMap for the alphabet <letter, id>
     private final HashMap<Character, Integer> alphabetMap = new HashMap<>();
+    //HashMap for the inverse alphabet <id, letter>
     private final HashMap<Integer, Character> inverseAlphabetMap = new HashMap<>();
+    //Alphabet String
     private final String alphabet;
 
-    public Alphabet(){
+    public Alphabet() {
 
+        //Initialise alphabet String
         this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+        //Create the alphabet
         this.generateAlphabet(this.alphabet);
 
     }
 
-    private void generateAlphabet(String alphabet){
+    /**
+     * Creates the alphabet in an HashMap form from the Alphabet String
+     *
+     * @param alphabet, String with the charater set
+     */
+
+    private void generateAlphabet(String alphabet) {
+        //Counter
         int count = 0;
 
-        for(char i : alphabet.toCharArray()){
+        //Create both regular and inverse alphabet from the char array of the alphabet string
+        for (char i : alphabet.toCharArray()) {
+            //Add the current letter into the alphabet
             alphabetMap.put(i, count);
+            //Add the current letter into the inverse alphabet
             inverseAlphabetMap.put(count, i);
+            //Increment counter
             count++;
         }
     }
+
+    /**
+     * Get the alphabet
+     *
+     * @return alphabetMap, Hashmap of the alphabet
+     */
 
     public HashMap<Character, Integer> getAlphabet() {
         return alphabetMap;
     }
 
-    public HashMap<Integer, Character> getInverseAlphabet(){
+    /**
+     * Get the inverse alphabet
+     *
+     * @return inverseAlphabetMap, Hashmap of the inverse alphabet
+     */
+
+    public HashMap<Integer, Character> getInverseAlphabet() {
         return inverseAlphabetMap;
     }
 }
--- src/main/java/org/VigenereCipher/VigenereDecryptNoKey.java
+++ src/main/java/org/VigenereCipher/VigenereDecryptNoKey.java
@@ -8,41 +8,76 @@ import java.util.HashMap;
 import java.io.IOException;
 
 
+/**
+ * Vigenere Cipher Decrypt
+ * When only 2 ciphertexts and a large pool of plaintexts are given
+ *
+ * @author Aran Jannson
+ */
+
 public class VigenereDecryptNoKey {
 
+    //Ciphertext 1 and 2
     String c1, c2;
+    //HashMap with the alphabet <letter, id>
     HashMap<Character, Integer> alphabet;
+    //HashMap with the inverse alphabet <id, letter>
     HashMap<Integer, Character> inverseAlphabet;
+    //Alphabet Object
     Alphabet alphabetClass = new Alphabet();
 
-    public VigenereDecryptNoKey(){
+    public VigenereDecryptNoKey() {
 
+        //Initialise the alphabet
         alphabet = alphabetClass.getAlphabet();
+        //Initialise the inverse alphabet
         inverseAlphabet = alphabetClass.getInverseAlphabet();
     }
 
+    /**
+     * Using the difference between 2 ciphertexts c1 and c2
+     * Then taking it away from the Integer representation of Plaintexts in '10letterwordslist.txt'
+     * It returns Plaintext 2 there for decrypting c2 which then gets c1 and the key respectively
+     *
+     * @param c1, ciphertext 1
+     * @param c2, ciphertext 2
+     * @throws FileNotFoundException, File was unable to opened
+     */
+
     public void decryptNoKey(String c1, String c2) throws FileNotFoundException {
 
+        //File with plaintext
         String file = "10letterwordslist.txt";
 
+        //Array of the difference between c1 and c2
         ArrayList<Integer> c3 = compare(stringToIntegerArray(c1), stringToIntegerArray(c2));
 
         try {
+            //BufferReader Declaration
             BufferedReader reader = new BufferedReader(new FileReader(file));
 
+            //String value for current line
             String line;
 
-            while((line = reader.readLine()) != null) {
+            //Loop through the file
+            while ((line = reader.readLine()) != null) {
 
+                //Compare the current line to the difference of c1 - c2
                 ArrayList<Integer> plainText2 = compare(stringToIntegerArray(line), c3);
+                //String for plainText2 output
                 String theWord;
 
+                //plaintext2 as a String
                 theWord = charArrayToString(intArrayToCharArray(plainText2));
 
-                if(line.equals(theWord)){
+                //Check if the word is located in the file
+                if (line.equals(theWord)) {
+                    //If successfully print
                     System.out.println("Success!!! " + theWord + " is in the file key is: ");
+                    //End the program
                     System.exit(0);
-                }else{
+                } else {
+                    //If not found print
                     System.out.println("Failed: " + theWord + " Is not in the file");
                 }
 
@@ -50,84 +85,124 @@ public class VigenereDecryptNoKey {
             }
 
 
+            //Check for an IOException
         } catch (IOException e) {
+            //Print out 'IOException caught'
             System.out.println("IOException caught");
-        }catch (NullPointerException e){
+
+            //Check for NullPointerExceptions
+        } catch (NullPointerException e) {
+            //Print out 'END'
             System.out.println("END");
         }
 
 
     }
 
-    public ArrayList<Integer> compare(ArrayList<Integer> array1, ArrayList<Integer> array2){
+    /**
+     * Takes away 2 Arrays of Integers representing letters
+     * Returning the difference of the 2 Integer Arrays
+     *
+     * @param array1, c1
+     * @param array2, c2
+     * @return outputArray
+     */
+
+    public ArrayList<Integer> compare(ArrayList<Integer> array1, ArrayList<Integer> array2) {
+        //Initialise array to be returned
         ArrayList<Integer> outputArray = new ArrayList<>();
 
-        for(int i =0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
+            //Current letter to be fetched from array1
             int firstValue = array1.get(i);
+            //Current letter to be fetched from array2
             int secondValue = array2.get(i);
 
+            //Current values of array1 and array2 taken away from each other
             int firstMinusSecondVal = (firstValue - secondValue);
 
+            //Check for negative values and out of bounds values
             if (firstMinusSecondVal < 0) {
+                //If negative add 25
                 firstMinusSecondVal += 25;
             } else if (firstMinusSecondVal >= 26) {
+                //If bigger than the max alphabet character take away 25
                 firstMinusSecondVal -= 25;
             }
 
+            //Add the value found into an ArrayList outputArray
             outputArray.add(firstMinusSecondVal);
 
         }
 
-
+        //Return the ArrayList containing the shift difference between array1 and array2
         return outputArray;
     }
 
-    public ArrayList<Character> intArrayToCharArray(ArrayList<Integer> array){
+    /**
+     * Converts Integer ArrayLists into Integer ArrayLists
+     *
+     * @param array, inputted ArrayList
+     * @return outputArray
+     */
 
+    public ArrayList<Character> intArrayToCharArray(ArrayList<Integer> array) {
+        //Initialise array to be returned
         ArrayList<Character> outputArray = new ArrayList<>();
 
-        for(int i : array){
+        //Loop through input as an array of Integers
+        for (int i : array) {
+            //Add the current Integers into the ArrayList
             outputArray.add(inverseAlphabet.get(i));
         }
 
+        //Return the ArrayList
         return outputArray;
 
 
     }
 
-    public ArrayList<Integer> stringToIntegerArray(String input){
+    /**
+     * Turns Strings into an ArrayList of Characters
+     *
+     * @param input, The inputted string
+     * @return outputArray
+     */
 
+    public ArrayList<Integer> stringToIntegerArray(String input) {
+        //Initialise array to be returned
         ArrayList<Integer> outputArray = new ArrayList<>();
 
-        for(char i : input.toCharArray()){
+        //Loop through input as an array of characters
+        for (char i : input.toCharArray()) {
+            //Add the current character into the ArrayList
             outputArray.add(alphabet.get(i));
         }
 
+        //Return the ArrayList
         return outputArray;
 
 
     }
 
-    public String charArrayToString(ArrayList<Character> array){
+    /**
+     * Turns Character ArrayLists into Strings
+     *
+     * @return buffer
+     */
 
+    public String charArrayToString(ArrayList<Character> array) {
+        //Initialise StringBuilder to be returned
         StringBuilder buffer = new StringBuilder();
 
-        for(char i : array){
+        //Loop through the ArrayList
+        for (char i : array) {
+            //Add the current Character in the ArrayList to the StringBuilder
             buffer.append(i);
         }
 
+        //Return the StringBuilder as a String
         return buffer.toString();
-
-    }
-
-    public ArrayList<Integer> charArrayToIntArray(ArrayList<Character> array){
-        ArrayList<Integer> outputArray = new ArrayList<>();
-
-        for(char i : array){
-            outputArray.add(alphabet.get(i));
-        }
-
-        return outputArray;
 
     }
 }
