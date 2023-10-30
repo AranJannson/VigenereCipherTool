ee6e93afe2862cf94d45401ef22c7a2b5e48cde2
Aran <aransjannson@gmail.com>
2023-10-30 22:22:48 +0000

    Added LICESE and Updated Package

--- src/main/java/org/VigenereCipher/Alphabet.java
+++ src/main/java/org/VigenereCipher/Alphabet.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.VigenereCipher;
 
 import java.util.HashMap;
 
--- src/main/java/org/VigenereCipher/Main.java
+++ src/main/java/org/VigenereCipher/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.VigenereCipher;
 
 import java.io.FileNotFoundException;
 
